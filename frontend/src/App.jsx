e030c1fb468c39d425025037da7345911d88a94d
Ankush Kumar <ankushkumae467@gmail.com>
2025-01-03 16:36:08 +0530

    kya pata

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,12 @@ import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import HomePage from "./pages/HomePage"
 import CreatePage from "./pages/CreatePage"
+import Baby from"./components/Baby"
 const App = () => {
   return (
     <div className="harry">
+      <Baby></Baby>
+      <br></br>
       <Navbar></Navbar>
       <Routes>
       <Route path="/" element={<HomePage></HomePage>}></Route>
---
+++ frontend/src/components/Baby.jsx
@@ -0,0 +1,10 @@
+
+const Baby = () => {
+  return (
+    <div>
+      <h1>THis is a FIle</h1>
+    </div>
+  )
+}
+
+export default Baby
